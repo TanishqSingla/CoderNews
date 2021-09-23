4f3faf933d7d1f101c95a73c0570f5fac330b782
TanishqSingla <tanishqsingla00@gmail.com>
2021-09-23 14:49:08 +0530

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,20 @@ import LinkList from './LinkList';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import CreateLink from './CreateLink';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
-  return <CreateLink />
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className="flex pa1 justify-between nowrap orange">
-            <div className="flex flex=fixed black">
-                <Link to="/" className="ml1 no-underline black">new</Link>
-                <div className="ml1">|</div>
-                <Link to="/create" className="ml1 no-underline black">
-                    submit
-                </Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex pa1 justify-between nowrap orange">
+      <div className="flex flex=fixed black">
+        <Link to="/" className="ml1 no-underline black">
+          new
+        </Link>
+        <div className="ml1">|</div>
+        <Link to="/create" className="ml1 no-underline black">
+          submit
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
